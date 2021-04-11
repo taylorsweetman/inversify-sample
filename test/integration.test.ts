d5fdc3b9b15e05540fca55ca99cae29fe28e39ea
Taylor Sweetman <taylor.sweetman@gmail.com>
2021-04-11 18:15:18 -0400

    started work on integration test

---
+++ test/integration.test.ts
@@ -0,0 +1,19 @@
+import { ApiManager } from '../src/entities/ApiManager';
+import { Manager } from '../src/interfaces';
+import { myContainer } from '../src/inversify.config';
+import { TYPES } from '../src/types';
+import { isJson } from './api/functions';
+
+const ENDPOINT = 'https://jsonplaceholder.typicode.com';
+const PATH = '/todos';
+const manager: ApiManager = myContainer.get<Manager>(TYPES.Manager);
+
+test('integration test', async () => {
+	expect(manager).toBeTruthy;
+	expect(manager._client).toBeTruthy;
+	const data = await manager.fetchData(ENDPOINT, PATH);
+	expect(isJson(data));
+	const dataObj = JSON.parse(data);
+	expect(dataObj.length).toBe(200);
+	expect(dataObj[0].id).toBe(1);
+});
